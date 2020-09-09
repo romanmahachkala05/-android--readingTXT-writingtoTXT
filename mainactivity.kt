7f5b1619eafb56fc449e9fa1b56187b250968482
Roman <60145514+romanmahachkala05@users.noreply.github.com>
2020-09-09 09:45:33 +0300

    Update mainactivity.kt

--- mainactivity.kt
+++ mainactivity.kt
@@ -1,75 +1,53 @@
 	
 	
-	data class dclass1(val deviceName: String = "", val scriptText: String = "")
+	data class dclass1(val headerSmall: String = "", val headerSmallText: String = "")
 
-        //i thought its ok
         val dclasslistArray: MutableList<List<dclass1>> = ArrayList()
         for (i in 0..9) dclasslistArray.add(ArrayList())
         var dclasscount = 0
         var dcclassnamescount = 0
-        var devicenamehelp = ""
-        var scripttexthelp = listOf<String>()
+        var headerSmallH = ""
+        var headerSmallTextH = listOf<String>()
         var dclassnames = listOf<String>()
         val sdcard = Environment.getExternalStorageDirectory()
-        loadbands.setOnClickListener {
+        readfile.setOnClickListener {
             try {
-                var txtfile = File(sdcard.absolutePath,"bands.txt") if (txtfile.exists()) {
-                    try {
-                        var txtfileList = txtfile.readLines()
-                        var i = 0
-                        while (i < txtfileList.size){
+                var txtfile = File(sdcard.absolutePath,"filename.txt") 
+		    if (txtfile.exists()) {
+			var txtfileList = txtfile.readLines()
+			var i = 0
+			while (i < txtfileList.size){
 
-                            //i need to separate big headers with %
-                            if (txtfileList.get(i).startsWith("%")) {
-                                dclassnames += txtfileList.get(i).removePrefix("%")
-                                dcclassnamescount++
-                                if (dcclassnamescount > 1) dclasscount++
-                            }
+				    //i need to separate big headers with %
+			     if (txtfileList.get(i).startsWith("%")) {
+				dclassnames += txtfileList.get(i).removePrefix("%")
+				dcclassnamescount++
+				if (dcclassnamescount > 1) dclasscount++
+			     }
 
-                            //separate smaller headers (deviceName in data class) with @
-                            if (txtfileList.get(i).startsWith("@")) {
-                                devicenamehelp = txtfileList.get(i)
-                                scripttexthelp = emptyList()
-                                i++
-                                //fill deviceName with scriptText
-                                while (!txtfileList.get(i).startsWith("$")) {
-                                    scripttexthelp += txtfileList.get(i)
-                                    i++
-                                }
-                                dclasslistArray[dclasscount] = dclasslistArray[dclasscount] + dclass1(devicenamehelp.toString(), scripttexthelp.toString().replace(",","").replace(" ","\n"))
-
-                            }
-                            i++
-                        }
-                        //
-                        // textViewCurrentDevice.text = dclasslistArray[0].get(0).deviceName
-                        textViewCurrentDevice.text = dclasslistArray[1].get(1).scriptText.toString()
-                      //
-
-                    } catch (e: IOException) {
-                        Toast.makeText(this, "Что то пошло не так :(", Toast.LENGTH_SHORT).show()
-
-                    }
-
-                } else Toast.makeText(this, "Файл не был найден", Toast.LENGTH_SHORT).show()
-
-
-
-
-            }
-            catch (ioe: IOException) {
-                Toast.makeText(this, "не считался файл", Toast.LENGTH_SHORT).show()
-            }
+			     //separate smaller headers (deviceName in data class) with @
+			     if (txtfileList.get(i).startsWith("@")) {
+				  headerSmallH = txtfileList.get(i)
+				  headerSmallTextH = emptyList()
+				  i++
+				  //fill headerSmall with headerSmallText
+				  while (!txtfileList.get(i).startsWith("$")) {
+					headerSmallTextH += txtfileList.get(i)
+					i++
+				  }
+			      dclasslistArray[dclasscount] = dclasslistArray[dclasscount] + dclass1(headerSmallH.toString(), headerSmallTextH.toString().replace(",","").replace(" ","\n"))
+			      }
+			 i++      
+                         }
+                    } else Toast.makeText(this, "txt was not found", Toast.LENGTH_SHORT).show()
+            	}  catch (ioe: IOException) {
+           	Toast.makeText(this, "unable to read txt", Toast.LENGTH_SHORT).show()
+                }
         }
 
-
-
-
-
-        unloadbands.setOnClickListener {
+        createfile.setOnClickListener {
             try {
-                var file = File(sdcard.absolutePath, "script.txt")
-
+                var file = File(sdcard.absolutePath, "file1.txt")
                 val osw = OutputStreamWriter(FileOutputStream(file))
 		
 		//underconstruction
@@ -77,11 +55,8 @@
                 osw.flush()
                 osw.close()
                 Toast.makeText(this, "Script created", Toast.LENGTH_LONG).show()
-
-
-
             }
             catch (ioe: IOException) {
-                Toast.makeText(this, "не считался файл", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "unable to create txt", Toast.LENGTH_SHORT).show()
             }
         }
